37a0f4e540f9e2eab217bef4e7cda2c6b0da44eb
Anbarasan Gangadaran <anbarasan.gangadaran@velocix.com>
2021-11-30 16:27:51 +0530

    modification for stats generation

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
-Stats Mod for BombSquad
+# Stats Mod for BombSquad
 
 updated for v1.6.5
+
+## publishing the stats
+
+- copy the index.html to your webserver
+- change mysettings.stats_file path such that the stats.json file is adjacent to index.html
---
+++ dist/ba_root/mods/mysettings.py
@@ -0,0 +1,17 @@
+import json
+import os
+
+import _ba
+
+stats_file = "/var/www/html/stats.json"
+html_file = "/var/www/html/stats.html"
+server_name = "வாய்ச்சொல் வீரர்கள் (Vaaichol Veerargal)"
+python_path = _ba.env()["python_directory_user"]
+
+
+def get_stats():
+    stats = {}
+    if os.path.exists(stats_file):
+        with open(stats_file) as f:
+            stats = json.load(f)
+    return stats
